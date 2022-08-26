2f3e21480dac30820b7eb3216e24f810e1efac06
kimdonghyeon <sh2115521@naver.com>
2022-08-26 15:03:38 +0900

    feat : SiteUserController 추가 조회

--- src/main/java/com/ll/exam/app3/SiteUserController.java
+++ src/main/java/com/ll/exam/app3/SiteUserController.java
@@ -1,15 +1,19 @@
 package com.ll.exam.app3;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class SiteUserController {
 
-    @RequestMapping("/user/{id}")
-    public void create(@PathVariable Long id){
+    private final SiteUserRepository userRepository;
 
+    @RequestMapping("/user/{id}")
+    public SiteUser user(@PathVariable Long id) {
+        return userRepository.getQslUser(id);
     }
 
 }
